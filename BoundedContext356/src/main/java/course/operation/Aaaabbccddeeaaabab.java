c8044180ed6d4bd345faa1a67462c7b789bd035f
root <sanghoon01@uengine.org>
2021-08-04 08:32:04 +0000

    aaaa

---
+++ BoundedContext356/src/main/java/course/operation/Aaaabbccddeeaaabab.java
@@ -0,0 +1,18 @@
+package course.operation;
+
+public class Aaaabbccddeeaaabab extends AbstractEvent {
+
+    private Long id;
+
+    public Aaaabbccddeeaaabab(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
